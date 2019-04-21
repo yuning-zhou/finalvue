83a7c10b81fd646716f2100e0fc2e71c31a56a9f
Yuning Zhou <yuning.zhou@stern.nyu.edu>
2019-04-20 20:58:20 -0400

    Update posts.js

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -7,16 +7,27 @@ const movies = mongoose.model('Movies');
 
 const router = express.Router();
 
+// get movies
 router.get('/', async (req, res) => {
-    const movieLists = await loadMovies();
-    res.send(console.log(movieLists));
-    
+    res.send(await movies.find({}));
 })
 
-function loadMovies(){
-    return movies.find({});
-}
+// add a movie
+router.post('/', async (req, res) => {
+    await movies.insertMany({
+        name:req.body.name,
+        director:req.body.director,
+        rating: req.body.rating
+    });
+    res.status(201).send();
+})
 
-
+// delete a movie
+router.delete('/:name', async (req, res) => {
+    await movies.deleteOne({
+        name:req.params.name
+    });
+    res.status(200).send();
+})
 
 module.exports = router;
